942b9726bc373078a52783c32554056b7a0d62b4
Ibrhaim <54852400+mohdibrahim1995@users.noreply.github.com>
2024-10-13 13:38:12 +0000

    Nextjs SetUp

---
+++ nextjs_aceternity/src/app/chai/page.tsx
@@ -0,0 +1,9 @@
+export default function ChaiPage(){
+    return ( 
+        <div>
+            <h1>Hello world</h1>
+        </div>
+
+    )
+
+}
---
+++ nextjs_aceternity/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="text-2xl text-center">
+     <h1>Hello world of Ibrahim</h1>
+    </div>
+  );
+}
